3ee442d95c4da982410cb8a13b8b43053d1a4f1c
caw <dominik.wozniewicz@gmail.com>
2023-01-26 14:30:23 +0100

    Basic app design

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button style={styles.addGoalButton} title="Add Goal" />
+      </View>
+      <View style={styles.goalList}>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
     alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 10,
+    width: '70%',
+    marginRight: 5,
+  },
+  goalList: {
+    paddingTop: 15,
   },
 });
